acf2ee70fb7cca2580e64030b6cfe14ddff4ce6a
manvanth__07 <manvanthpatelkr@gmail.com>
2025-05-06 13:46:29 +0530

    new code

---
+++ new.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    // Displaying the menu options
+    printf("Welcome to the Menu!\n");
+    printf("Please choose an option:\n");
+    printf("1. Option 1\n");
+    printf("2. Option 2\n");
+    printf("3. Option 3\n");
+    printf("4. Option 4\n");
+    printf("5. Option 5\n");
+    printf("Enter your choice (1-5): ");
+    
